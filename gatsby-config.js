9de6716ef89ef08e60be1e78ed71e01febf4fc6f
philippe <akaphilippe@gmail.com>
2020-11-22 20:09:12 -0500

    adding support for i18n

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,33 @@
  */
 
 module.exports = {
-  /* Your site config here */
   plugins: [
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        path: `${__dirname}/locales`,
+        languages: [`en`, `fr`],
+        defaultLanguage: `en`,
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false,
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        pages: [
+          {
+            matchPath: "/",
+            languages: ["en", "fr"],
+          },
+        ],
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from "react"
-import { Button, Flex, Link } from "@chakra-ui/react"
+import { Button, Flex } from "@chakra-ui/react"
 import { Container, Row, Column } from "../components/layout"
+import { Link as NewLink, useTranslation } from "gatsby-plugin-react-i18next"
 import Text from "../components/Text"
 
 const Home = () => {
+  const { t } = useTranslation()
   return (
     <Container>
       <Row>
@@ -12,6 +14,7 @@ const Home = () => {
             <Text type="heading.first">
               Gatsby JS and Chakra UI template starter
             </Text>
+            <Text type="heading.second"> {t("welcome")}</Text>
           </Flex>
         </Column>
       </Row>
