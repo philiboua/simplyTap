f4d4060afd77fe5cd88252e07829aa9e816627ec
Philippe <akaphilippe@gmail.com>
2020-12-22 11:11:13 -0500

    poc for header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,14 @@ import { useTranslation } from "gatsby-plugin-react-i18next"
 import { Container, Row, Column } from "@components/layout"
 import Header from "@components/patterns/header"
 import HamburgerButton from "@components/button/HamburgerButton"
+import { Text } from "@chakra-ui/react"
 
 const Home = (): React.ReactElement => {
-  const { t } = useTranslation()
   return (
     <Container>
       <Row align="center">
-        <Column col={["sm4", "md3", "lg12"]}>
+        <Column col={["sm4", "md6", "lg12"]}>
           <Header />
-          <HamburgerButton
-            data={{
-              ariaLabel: "Navigation menu",
-              displayLabel: [true],
-              menuLabel: "MENU",
-            }}
-          />
         </Column>
       </Row>
     </Container>
