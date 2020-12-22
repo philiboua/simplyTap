8e96cf5915684af5d949b66f0789127feef4e01f
Philippe <akaphilippe@gmail.com>
2020-12-22 11:08:09 -0500

    updated breakpoints

--- src/chakra-ui/theme/foundations/breakpoints.js
+++ src/chakra-ui/theme/foundations/breakpoints.js
@@ -1,7 +1,7 @@
 import { createBreakpoints } from "@chakra-ui/theme-tools"
 
 export const breakpoints = createBreakpoints({
-  sm: "576px",
+  sm: "320px",
   md: "768px",
   lg: "992px",
   xl: "1200px",
