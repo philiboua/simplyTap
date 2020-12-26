37f8d3d8dd30b35c19d73052f9ea3b9726ebd26e
Philippe <akaphilippe@gmail.com>
2020-12-26 16:48:10 -0500

    created Billboard component

--- src/components/patterns/billboard/index.tsx
+++ src/components/patterns/billboard/index.tsx
@@ -5,7 +5,18 @@
  */
 
 import React from "react"
-import { Link as ChakraLink, LinkProps } from "@chakra-ui/react"
+
+import {
+  Link as ChakraLink,
+  LinkProps,
+  Box,
+  VStack,
+  HStack,
+} from "@chakra-ui/react"
+import { Container, Column, Row } from "@components/layout"
+import Text from "@components/Text"
+import ListOfLinks from "@components/listOfLinks"
+import Img from "gatsby-image"
 
 interface ICustomLinkProps extends LinkProps {
   asButton?: boolean
@@ -18,11 +29,48 @@ interface IBillboard {
   headline: string
   content: string
   callToAction: ICustomLinkProps[]
-  image: string
+  image?: {
+    fluid: {
+      aspectRatio: number
+      base64: string
+      src: string
+      srcSet: string
+      sizes: string
+    }
+  }
 }
 
-const Billboard: React.FC<IBillboard> = () => {
-  return <div>Hello</div>
+const Billboard: React.FC<IBillboard> = ({
+  caption,
+  headline,
+  content,
+  callToAction,
+  image,
+}) => {
+  return (
+    <Box>
+      <Container>
+        <Row>
+          <Column>
+            <VStack
+              alignItems={image === undefined ? "center" : "flex-start"}
+              pt={12}
+            >
+              {caption && <Text type="caption">{caption}</Text>}
+              <Text type="heading.first">{headline}</Text>
+              <Text type="body.first">{content}</Text>
+              <ListOfLinks content={callToAction} />
+            </VStack>
+          </Column>
+          {image !== undefined && (
+            <Column>
+              <Img fluid={image.fluid} alt="billboard Image" />
+            </Column>
+          )}
+        </Row>
+      </Container>
+    </Box>
+  )
 }
 
 export default Billboard
